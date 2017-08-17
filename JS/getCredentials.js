d8d0bca9d2bc2988e45307755dd7f8ea44ea63d2
Eugen Stotik <eugen_stotik@epam.com>
2017-08-17 14:33:34 +0300

    Log

--- JS/getCredentials.js
+++ JS/getCredentials.js
@@ -1,5 +1,7 @@
 console.log("I'll got your data, bitch!");
 
+console.log("args size: ", process.argv);
+
 process.argv.forEach(function (val, index, array) {
   console.log(index + ': ' + val);
 });
